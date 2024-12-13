fb3104fb98a8d5d027aab3eb93f0e672ccf8ee9a
Marc Anton Dahmen <mail@marcdahmen.de>
2024-12-13 18:30:47 +0100

    feat: disable meta tags extension in version 2

--- templates/elements/metatags.php
+++ templates/elements/metatags.php
@@ -1,9 +1,16 @@
 <@ set_teaser_variable.php @>
+<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
 <@ set { :description: @{ metaDescription | def(@{ :teaser | stripTags }) } } @>
 <@ Automad/MetaTags { 
 	description: @{ :description },
 	ogTitle: @{ metaTitle | def('@{ sitename } / @{ title | def ("404") }') },
 	ogDescription: @{ :description },
 	ogType: 'website',
-	ogImage: @{ openGraphImage }
+	ogImage: @{ 
+		ogImage | 
+		def('@{ +hero | findFirstImage }') | 
+		def('@{ +main | findFirstImage }') | 
+		def('*.jpg, *.png, *.gif, /shared/*.jpg, /shared/*.png, /shared/*.gif') 
+	}
 } @>
+<?php } ?>
