2b53c7e23c103acb75e6a82a69adbae2e0220c04
Marc Anton Dahmen <mail@marcdahmen.de>
2024-12-14 10:36:34 +0100

    fix: remove duplicate charset meta tag

--- templates/post.php
+++ templates/post.php
@@ -2,7 +2,6 @@
 <html lang="en" class="@{ theme | sanitize }">
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
 		<title>@{ metaTitle | def('@{ sitename } / @{ title | def ("404") }') }</title>
